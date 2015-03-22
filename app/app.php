1d0ecd5c1d8fe50ae83560c9eb4797ed7047de27
Bryan <bryan.borgeson@gmail.com>
2015-03-22 10:37:15 -0700

    Add commented feature to quickly link a psql db to app.php

--- app/app.php
+++ app/app.php
@@ -4,14 +4,26 @@
     require_once __DIR__.'/../src/MyProjectClass.php';
 
 
-    // ***UNCOMMENT IF YOUR PROJECT NEEDS TO SAVE DATA INTO COOKIES***
+    // --->>> UNCOMMENT IF YOUR PROJECT WILL SAVE DATA INTO COOKIES <<< --- //
+    //
     // session_start();
     //
     // if(empty($_SESSION['my_session'])) {
     //     $_SESSION['my_session'] = array();
     // }
+    //
+    // --------------------------->>> END <<< ----------------------------- //
+
+
+    // ------>>> UNCOMMENT IF YOUR PROJECT WILL INTERFACE W/ A DB <<<------ //
+    //
+    // $DB = new PDO('pgsql:host=localhost; dbname=$DB_NAME);
+    //
+    // --------------------------->>> END <<< ----------------------------- //
+
 
     $app = new Silex\Application;
+
     $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
