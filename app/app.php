3dab2ab1669b819b1f38c268134d0f187842cd9e
geoff winner & Kyle Giard-Chase <student@epicodus.com>
2015-03-24 16:48:41 -0700

    add routing and basic navigation/rendering

--- app/app.php
+++ app/app.php
@@ -1,37 +1,51 @@
 <?php
 
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../src/MyProjectClass.php';
+    require_once __DIR__.'/../src/Course.php';
+    require_once __DIR__.'/../src/Student.php';
 
 
-    // --->>> UNCOMMENT IF YOUR PROJECT WILL SAVE DATA INTO COOKIES <<< --- //
-    //
-    // session_start();
-    //
-    // if(empty($_SESSION['my_session'])) {
-    //     $_SESSION['my_session'] = array();
-    // }
-    //
-    // --------------------------->>> END <<< ----------------------------- //
+    $DB = new PDO('pgsql:host=localhost; dbname=uofu');
 
-
-    // ------>>> UNCOMMENT IF YOUR PROJECT WILL INTERFACE W/ A DB <<<------ //
-    //
-    // $DB = new PDO('pgsql:host=localhost; dbname=$DB_NAME);
-    //
-    // --------------------------->>> END <<< ----------------------------- //
-
-
-    $app = new Silex\Application;
+    $app = new Silex\Application();
 
     $app['debug'] = true;
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path'=>__DIR__.'/../views'
     ));
 
     $app->get('/', function() use ($app) {
-        return $app['twig']->render('template.twig');
+        return $app['twig']->render('index.twig', array('courses' => Course::getAll()));
+    });
+
+    $app->get('/courses', function() use ($app) {
+        return $app['twig']->render('courses.twig', array('courses' => Course::getAll()));
+    });
+
+    $app->post('/courses', function() use ($app) {
+        $course = $_POST['course'];
+        $coursenumber = $_POST['coursenumber'];
+        $new_course = new Course($course, $coursenumber);
+        $new_course->save();
+        return $app['twig']->render('courses.twig', array('courses' => Course::getAll()));
+    });
+
+    $app->get('/courses/{id}', function($id) use ($app) {
+        $course = Course::find($id);
+        return $app['twig']->render('course.twig', array('course' => $course, 'courses' => Course::getAll(), 'students' => Student::getAll()));
+    });
+
+    $app->post('/courses/{id}', function($id) use($app) {
+        $course = Course::find($id);
+        $name = $_POST['name'];
+        $date = $_POST['date'];
+        $new_student = new Student($name, $date);
+        $new_student->save();
+        return $app['twig']->render
     });
 
     return $app;
